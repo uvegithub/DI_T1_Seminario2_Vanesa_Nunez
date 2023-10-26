e08ad3ada9a6bbfdc632aee5e3324cb03a92e5bf
Vanesa <vanesanunez90dos@gmail.com>
2023-10-26 18:58:31 +0200

    26.10.23

---
+++ app/src/main/java/com/example/di_t1_seminario2_vanesa_nunez/Ejercicio_1.kt
@@ -0,0 +1,11 @@
+package com.example.di_t1_seminario2_vanesa_nunez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio1)
+    }
+}
